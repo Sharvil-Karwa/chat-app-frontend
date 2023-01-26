461a84371049199e4a190cb0199dc88b2cebcd25
Sharvil <karwasharvil@gmail.com>
2023-01-26 23:58:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { io } from "socket.io-client";
+import { Container } from "@mui/system";
+import { Typography } from "@mui/material";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [msg, setMsg] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("msg-from-server", (msg) => {
+      setChat((chat) => [...chat, msg.msg]);
+    });
+  }, [socket]);
+
+  function handleForm(e) {
+    e.preventDefault();
+    socket.emit("send-msg", { msg });
+    setMsg("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box sx={{ marginBottom: 5 }}>
+        {chat.map((message) => (
+          <Typography>{message}</Typography>
+        ))}
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleForm}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+        />
+        <Button variant="contained" type="submit">
+          SUBMIT
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
